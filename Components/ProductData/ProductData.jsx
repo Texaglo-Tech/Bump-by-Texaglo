227cbf570d58416438af7716fb1dcb9947eed61b
Det-tech <proninkirilll228@gmail.com>
2023-10-03 12:23:35 -0700

    fixed membership field condition in ProductData

--- Components/ProductData/ProductData.jsx
+++ Components/ProductData/ProductData.jsx
@@ -6,55 +6,78 @@ import Style from "./ProductData.module.css";
 import images from "../../assets";
 
 import { getProductSummary } from "../../api";
-import { addMembershipDiscount, exportToExcel, getUserIdFromToken } from "../../api";
+import {
+  addMembershipDiscount,
+  exportToExcel,
+  getUserIdFromToken,
+} from "../../api";
+import { toast } from "react-toastify";
 
 const ProductData = () => {
-
   const [summary_data, setSummaryData] = useState({
     scans_cnt: 0,
     clicks_cnt: 0,
     online_sales_cnt: 0,
     local_sales_cnt: 0,
-    fee: '',
-    percent: ''
-  })
+    fee: "",
+    percent: "",
+  });
 
-  useEffect(()=>{
+  useEffect(() => {
     const data = {
       user_id: getUserIdFromToken(),
-    }
-    getProductSummary(data).then(res=>{
-      if(res.success){
+    };
+    getProductSummary(data).then((res) => {
+      if (res.success) {
         setSummaryData({
           ...summary_data,
           scans_cnt: res.data.scans_cnt,
           clicks_cnt: res.data.clicks_cnt,
           online_sales_cnt: res.data.online_sales_cnt,
-          local_sales_cnt: res.data.local_sales_cnt
-        })
+          local_sales_cnt: res.data.local_sales_cnt,
+        });
       }
-    })
-  }, [])
+    });
+  }, []);
 
-  const exportData = async()=> {
-    console.log("export Data...")
-    const data = [{Scans: summary_data.scans_cnt, Clicks: summary_data.clicks_cnt, "Online Sales": summary_data.online_sales_cnt, "Local Sales": summary_data.local_sales_cnt,      
-    }]
-    exportToExcel("data", data)    
-  }
+  const exportData = async () => {
+    console.log("export Data...");
+    const data = [
+      {
+        Scans: summary_data.scans_cnt,
+        Clicks: summary_data.clicks_cnt,
+        "Online Sales": summary_data.online_sales_cnt,
+        "Local Sales": summary_data.local_sales_cnt,
+      },
+    ];
+    exportToExcel("data", data);
+  };
 
-  const addMemberDiscount = async()=>{
-    console.log("addMemberDiscount...")
-    
+  const addMemberDiscount = async () => {
+    console.log("addMemberDiscount...");
+    console.log(summary_data);
+    if (summary_data.fee == "") {
+      toast.warning("Please input fee!", {
+        position: toast.POSITION.TOP_RIGHT,
+        autoClose: 3000,
+      });
+      return;
+    }
+    if (summary_data.percent == "") {
+      toast.warning("Please input percent!", {
+        position: toast.POSITION.TOP_RIGHT,
+        autoClose: 3000,
+      });
+      return;
+    }
     const data = {
       user_id: getUserIdFromToken(),
       product_id: 1,
       fee: summary_data.fee,
-      percent: summary_data.percent
-    }
-    await addMembershipDiscount(data)
-
-  }
+      percent: summary_data.percent,
+    };
+    await addMembershipDiscount(data);
+  };
 
   return (
     <>
@@ -85,7 +108,12 @@ const ProductData = () => {
               <span>Local Sales</span>
             </div>
           </div>
-          <button onClick={exportData} className={Style.Product_data_export_btn}>export data</button>
+          <button
+            onClick={exportData}
+            className={Style.Product_data_export_btn}
+          >
+            export data
+          </button>
         </div>
         <div className={Style.Product_post_card_section}>
           <div className={Style.Product_post_card_box}>
@@ -102,14 +130,35 @@ const ProductData = () => {
 
               <div className={Style.Product_card_btn_box}>
                 <button>Fee</button>
-                <input placeholder="Price"/>
+                <input
+                  placeholder="Price"
+                  onChange={(e) =>
+                    setSummaryData({
+                      ...summary_data,
+                      fee: e.target.value,
+                    })
+                  }
+                  required
+                />
               </div>
 
               <div className={Style.Product_card_btn_box}>
                 <button>Discount</button>
-                <input placeholder="Percent"/>
+                <input
+                  placeholder="Percent"
+                  onChange={(e) =>
+                    setSummaryData({
+                      ...summary_data,
+                      percent: e.target.value,
+                    })
+                  }
+                  required
+                />
               </div>
-              <div  onClick={addMemberDiscount} className={Style.Product_card_download_btn}>
+              <div
+                onClick={addMemberDiscount}
+                className={Style.Product_card_download_btn}
+              >
                 Add member discount
               </div>
             </div>
