d709da24799cd58fc586bb0e842efefc0f66fde2
Det-tech <proninkirilll228@gmail.com>
2023-10-03 16:27:44 -0700

    renamed first Uppercase to lowercase of classname in projectSections

--- Components/ProjectsSections/ProjectsSections.jsx
+++ Components/ProjectsSections/ProjectsSections.jsx
@@ -2,15 +2,14 @@ import React from "react";
 import Image from "next/image";
 
 //INTERNAL IMPORT
-import Style from "./ProjectsSections.module.css";
-import images from "../../assets";
+import style from "./ProjectsSections.module.css";
 import ProjectCard from "../ProjectCard/ProjectCard";
 
 const ProjectsSections = () => {
   return (
-    <div className={Style.project_Section}>
-      <h1 className={Style.project_Section_title}>Projects</h1>
-      <div className={Style.project_card_list}>
+    <div className={style.project_section}>
+      <h1 className={style.project_section_title}>Projects</h1>
+      <div className={style.project_card_list}>
         <ProjectCard />
         <ProjectCard />
         <ProjectCard />
