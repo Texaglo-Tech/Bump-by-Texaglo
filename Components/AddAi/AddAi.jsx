91eb2fc945c78abf420da20e2c9f8586c078c39b
Det-tech <proninkirilll228@gmail.com>
2023-10-04 04:53:17 -0700

    fixed responsive issue in AddAi

--- Components/AddAi/AddAi.jsx
+++ Components/AddAi/AddAi.jsx
@@ -8,6 +8,7 @@ import Switch from "@mui/material/Switch";
 import { styled } from "@mui/material/styles";
 import { useGlobal } from "../../context/GlobalContext";
 import { aiDeploy } from "../../api";
+import { Grid } from '@mui/material';
 
 const AntSwitch = styled(Switch)(({ theme }) => ({
   width: "80px",
@@ -66,109 +67,115 @@ const AddAi = () => {
   return (
     <>
       <div className={Style.product_section}>
-        <div className={Style.product_post_section}>
-          <h1>Add Ai to Bump-me</h1>
-          <div className={Style.product_post_input}>
-            <div className={Style.toggle_switch_container}>
-              <div
-                style={{
-                  display: "flex",
-                  marginLeft: "-30px",
-                  marginTop: "30px",
-                }}
-              >
-                <AntSwitch
-                  onChange={() => {
-                    productDataHandle("ai", product_data.ai ? false : true);
-                  }}
-                  inputProps={{ "aria-label": "ant design" }}
-                />
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={6} sx={{padding:"30px"}}>
+            <div className={Style.product_post_section}>
+              <h1>Add Ai to Bump-me</h1>
+              <div className={Style.product_post_input}>
+                <div className={Style.toggle_switch_container}>
+                  <div
+                    style={{
+                      display: "flex",
+                      marginLeft: "-30px",
+                      marginTop: "30px",
+                    }}
+                  >
+                    <AntSwitch
+                      onChange={() => {
+                        productDataHandle("ai", product_data.ai ? false : true);
+                      }}
+                      inputProps={{ "aria-label": "ant design" }}
+                    />
 
+                    <div
+                      style={{
+                        marginLeft: "-15px",
+                        marginTop: "-5px",
+                        display: "flex",
+                        flexDirection: "column",
+                      }}
+                    >
+                      <label
+                        style={{ fontSize: "17px", color: "white" }}
+                        onClick={() => {
+                          productDataHandle("ai", true);
+                        }}
+                      >
+                        On
+                      </label>
+                      <span style={{ height: "10px" }}></span>
+                      <label
+                        style={{ fontSize: "17px", color: "white" }}
+                        onClick={() => {
+                          productDataHandle("ai", false);
+                        }}
+                      >
+                        Off
+                      </label>
+                    </div>
+                  </div>
+                  <div
+                    style={{
+                      alignItems: "center",
+                      textAlign: "center",
+                      display: "flex",
+                      padding: "20px",
+                      marginTop: "20px",
+                    }}
+                  >
+                    admin can turn the ai on or off which on their products
+                  </div>
+                </div>
+              </div>
+              <div className={Style.product_file_input_box}>
                 <div
-                  style={{
-                    marginLeft: "-15px",
-                    marginTop: "-5px",
-                    display: "flex",
-                    flexDirection: "column",
-                  }}
+                  className={Style.product_file_input}
+                  onClick={() => fileInputRef.current.click()}
+                  onChange={() => setFile(e.target.files[0])}
                 >
-                  <label
-                    style={{ fontSize: "17px", color: "white" }}
-                    onClick={() => {
-                      productDataHandle("ai", true);
-                    }}
-                  >
-                    On
-                  </label>
-                  <span style={{ height: "10px" }}></span>
-                  <label
-                    style={{ fontSize: "17px", color: "white" }}
-                    onClick={() => {
-                      productDataHandle("ai", false);
-                    }}
-                  >
-                    Off
-                  </label>
+                  <Image className={Style.circle} src={images.circle} alt="image" />
+                  <span>+</span>
+                  <p>Add Product info pdf</p>
+                  <input
+                    type="file"
+                    ref={fileInputRef}
+                    style={{ display: "none" }}
+                  />
                 </div>
               </div>
-              <div
-                style={{
-                  alignItems: "center",
-                  textAlign: "center",
-                  display: "flex",
-                  padding: "20px",
-                  marginTop: "20px",
-                }}
-              >
-                admin can turn the ai on or off which on their products
-              </div>
             </div>
-          </div>
-          <div className={Style.product_file_input_box}>
-            <div
-              className={Style.product_file_input}
-              onClick={() => fileInputRef.current.click()}
-              onChange={() => setFile(e.target.files[0])}
-            >
-              <Image className={Style.circle} src={images.circle} alt="image" />
-              <span>+</span>
-              <p>Add Product info pdf</p>
-              <input
-                type="file"
-                ref={fileInputRef}
-                style={{ display: "none" }}
-              />
-            </div>
-          </div>
-        </div>
-        <div className={Style.product_post_card_section}>
-          <div className={Style.product_post_card_box}>
-            <h1>Preview </h1>
-            <div className={Style.product_post_card}>
-              <h3
-                style={{ color: "white", padding: "20px", textAlign: "center" }}
-              >
-                the ai is overlayed on top of the application when the user says
-                the keyword “Help Me”
-              </h3>
+          </Grid>
+          <Grid item xs={12} md={6} sx={{padding:"30px"}}>
+            <div className={Style.product_post_card_section}>
+              <div className={Style.product_post_card_box}>
+                <h1>Preview </h1>
+                <div className={Style.product_post_card}>
+                  <h3
+                    style={{ color: "white", padding: "20px", textAlign: "center" }}
+                  >
+                    the ai is overlayed on top of the application when the user says
+                    the keyword “Help Me”
+                  </h3>
 
-              <div className={Style.product_post_card_img_box}>
-                <div className={Style.product_post_card_img_content}>
-                  <h3 style={{ color: "black" }}>How can I assist ?</h3>
-                  <div style={{ marginTop: "30px" }}>
-                    <Image src={images.ai_help} alt="image" />
+                  <div className={Style.product_post_card_img_box}>
+                    <div className={Style.product_post_card_img_content}>
+                      <h3 style={{ color: "black" }}>How can I assist ?</h3>
+                      <div style={{ marginTop: "30px" }}>
+                        <Image src={images.ai_help} alt="image" />
+                      </div>
+                      <div className={Style.product_post_card_img_content_btn_two}>
+                        <button>Casper is Thinking</button>
+                      </div>
+                    </div>
                   </div>
-                  <div className={Style.product_post_card_img_content_btn_two}>
-                    <button>Casper is Thinking</button>
+                  <div className={Style.card_save_btn} onClick={aiDeployHandle}>
+                    Save and Deploy
                   </div>
                 </div>
               </div>
-              <div className={Style.card_save_btn} onClick={aiDeployHandle}>
-                Save and Deploy
-              </div>
             </div>
-          </div>
-        </div>
+          </Grid>
+        </Grid>
       </div>
     </>
   );
