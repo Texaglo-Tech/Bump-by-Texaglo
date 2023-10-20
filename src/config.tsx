0de8b5ddc28189d997aee72e5e7f01e48669ab93
Texaglo-Technoking <keyvelbusiness@gmail.com>
2023-10-20 13:37:13 -0500

    updated UI removed some Console logs, lowered the volume of the pings for the AI Bot,added stripe into the app

--- src/config.tsx
+++ src/config.tsx
@@ -2,12 +2,12 @@ export default {
   "API": "https://crossmint-backend.texaglo.com",
   "API1": "http://192.168.109.239:7000",
   "FRONTEND_URL":"https://solana-texaglo.vercel.app",
-  "GMAIL": "technoking@gmail.com",
+  "GMAIL": "email",
   "WHATSAPP": "whatsapp://send?phone=1234123",
-  "TWITTER": "https://twitter.com/technoking",
+  "TWITTER": "https://twitter.com/texaglo_techno",
   "MESSAGES": "https://message.com",
   "FACEBOOK": "https://facebook.com",
   "CROSSMINT_PAYMENT": "https://crossmint.texaglo.com",
-  "STRIPE_PUBKEY": "pk_test_51NuIKtGSeIXWr6sy8zvifdWd8DUksZOIyfndl4FQW71WYfwK9tgfeQPkmKztAKXdSbNDsQOtEQnWsmpsHjBsV6vU00Y4IYLbhj",
-  "STRIPE_SECKEY": "sk_test_51NuIKtGSeIXWr6sy4GqZ6nzwi0cfbsnKE7RlaMIulgkHmlS4eugoKdSlwWojhDS64vpAEj9LxQzdNfRtK2UDeOp5004Pv7i0hf"
+  "STRIPE_PUBKEY": "stripekeys",
+  "STRIPE_SECKEY": "stripePublic_keys"
 }
